80a7150e519242d81a248acfb3d558f21d07755a
Tomas Balaz <balaztomas0@gmail.com>
2022-07-18 13:12:06 +0200

    Created bookmark-api application

---
+++ src/main/java/sk/balaz/bookmarkapi/BookmarkApiApplication.java
@@ -0,0 +1,13 @@
+package sk.balaz.bookmarkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmarkApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookmarkApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sk/balaz/bookmarkapi/BookmarkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.balaz.bookmarkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmarkApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
