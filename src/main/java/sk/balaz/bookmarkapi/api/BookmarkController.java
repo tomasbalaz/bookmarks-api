1cbc1eeabbc1713b3c09b6bdb48f6ebb90307d60
Tomas Balaz <balaztomas0@gmail.com>
2022-07-18 13:41:40 +0200

    Created BookmarkController.java and exposed REST API GET /api/v1/bookmark

---
+++ src/main/java/sk/balaz/bookmarkapi/api/BookmarkController.java
@@ -0,0 +1,23 @@
+package sk.balaz.bookmarkapi.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sk.balaz.bookmarkapi.domain.Bookmark;
+import sk.balaz.bookmarkapi.domain.BookmarkService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/bookmark")
+@RequiredArgsConstructor
+public class BookmarkController {
+
+    private final BookmarkService bookmarkService;
+
+    @GetMapping
+    public List<Bookmark> getBookmarks() {
+        return bookmarkService.getBookmarks();
+    }
+}
