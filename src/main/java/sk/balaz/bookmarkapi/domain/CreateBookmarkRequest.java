98e0dbf79fc8101ae1e04cf2b1529d5d92ff129d
Tomas Balaz <balaztomas0@gmail.com>
2022-08-16 14:52:04 +0200

    Exposed REST POST /api/v1/bookmark

---
+++ src/main/java/sk/balaz/bookmarkapi/domain/CreateBookmarkRequest.java
@@ -0,0 +1,17 @@
+package sk.balaz.bookmarkapi.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+
+@Setter
+@Getter
+public class CreateBookmarkRequest {
+
+    @NotEmpty(message = "Title should not be empty")
+    private String title;
+
+    @NotEmpty(message = "Url should not be empty")
+    private String url;
+}
